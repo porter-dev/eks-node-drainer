9bf8608018dbb2061a752f8750d8e37f7129594a
Rudi MK <rudimk.cloud@gmail.com>
2022-09-25 14:15:15 +0200

    Added variables.

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "aws_access_key" {
+  type = string
+}
+
+variable "aws_secret_key" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable cluster_endpoint {
+    type = string
+}
+
+variable cluster_ca_data {
+    type = string
+}
+
+variable "asg_arns" {
+    type = list(string)
+}
+
+variable "asg_names" {
+    type = list(string)
+}
